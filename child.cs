de81da4a761648e5768a7c5455ef939cecd3f016
Hanna kahehu <39971210+hannakahehu@users.noreply.github.com>
2021-09-28 09:37:49 +0000

    Initial commit

---
+++ child.cs
@@ -0,0 +1,20 @@
+
+// using System;
+
+// class Child:Mother
+// {
+//  public static void  Main (string  [] args)  {
+//  Console.WriteLine("Hello child");
+//  Child mychild =newChild();
+//  Console.WriteLine(mychild.name);
+//  }
+// }
+
+using System;
+
+
+  class Car : Vehicle  // Derived class
+  {
+    public string modelName = "Mustang";  // Car field
+  }
+
---
+++ main.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+  class Vehicle  // Base class
+  {
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+      Console.WriteLine("Tuut, tuut!");
+    }
+  }
+
+  
+  
